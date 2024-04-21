1fe34fa91d24bf1d9e238da68136b63046e0c624
Cristobal Barberis <cbarberis@adaptivecomputing.com>
2024-04-21 03:14:52 -0400

    just beautier

--- public/nosw_app.js
+++ public/nosw_app.js
@@ -8,6 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const readFileAsync = promisify(fs.readFile);
 
+let model = null;
+
 const CLASS_NAMES = [
     'carcharhinus_leucas',
     'carcharodon_carcharias', 
@@ -43,7 +45,7 @@ async function loadTrainingData() {
 }
 
 function defineModel() {
-    const model = tf.sequential();
+    model = tf.sequential();
     model.add(tf.layers.conv2d({
         inputShape: [224, 224, 3],
         filters: 32,
@@ -74,7 +76,7 @@ function defineModel() {
     return model;
 }
 
-async function main() {
+async function loadAndTrain() {
     const { xs, ys } = await loadTrainingData();
     const model = defineModel();
     await model.fit(xs, ys, {
@@ -84,5 +86,43 @@ async function main() {
     });
     console.log('Model trained successfully!');
 }
+
+
+async function classifyNewImage(imagePath) {
+  const image = await loadAndProcessImage(imagePath);
+  const prediction = model.predict(image.expandDims()); // Ensure the image has the batch dimension
+  const probabilities = prediction.dataSync();
+  const results = CLASS_NAMES.map((className, index) => ({
+      className,
+      probability: probabilities[index].toFixed(4)
+  }));
+  results.forEach(result => console.log(`${result.className}: ${result.probability}`));
+  return results;
+}
+
+
+
+async function main() {
+  await loadAndTrain();
+  console.log('>>> Test Galeocerdo Cuvier image <<<');
+  await classifyNewImage('images/dataSets/sharks/galeocerdo_cuvier/VP UF-TRO8935 lingual copy.png');
+  console.log('');
+  console.log('>>> Test Carcharodon Carcharias image <<<');
+  await classifyNewImage('images/dataSets/sharks/carcharodon_carcharias/VP UF-TRO2367 lingual copy.png');
+  console.log('');
+  console.log('>>> Test Hemipristis Serra image <<<');
+  await classifyNewImage('images/dataSets/sharks/hemipristis_serra/VP UF-TRO 7575 lingual copy.png');
+  console.log('');
+  console.log('>>> Test Carcharhinus Leucas image <<<');
+  await classifyNewImage('images/dataSets/sharks/carcharhinus_leucas/VP UF64381E lingual copy.png');
+  console.log('');
+  console.log('>>> Test Otodus Megalodon image <<<');
+  await classifyNewImage('images/dataSets/sharks/otodus_megalodon/VP UF-TRO4486 lingual copy.png');
+  console.log('');
+  console.log('>>> Test Isurus Hastalis image <<<');
+  await classifyNewImage('images/dataSets/sharks/isurus_hastalis/VP UF-TRO7321 lingual copy.png');
+
+}
+
 
 main().catch(console.error);
